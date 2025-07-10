61d82a4bc2ff9df7e9a6f8187bb45f065137c124
eryk <eryk.czyzak@gmail.com>
2025-07-10 17:42:52 +0200

    add ellipse rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <raylib.h>
+#include <rlgl.h>
+#include <glm/glm.hpp>
 #include <iostream>
+constexpr int ELLIPSE_VERTICES = 100;
+constexpr double EARTH_EQUATORIAL_RADIUS = 6378137.0;
+constexpr double RENDER_SCALE = 1.0 / EARTH_EQUATORIAL_RADIUS;
+
+std::vector<glm::vec3> ellipse(float semiMajorAxis, float semiMinorAxis) {
+	constexpr float step{ 2 * PI / ELLIPSE_VERTICES };
+	std::vector<glm::vec3> vertices{};
+	vertices.reserve(ELLIPSE_VERTICES);
+	float theta{ 0.0f };
+	for(int i = 0; i < ELLIPSE_VERTICES; ++i) {
+		vertices.emplace_back(semiMajorAxis * cosf(theta), semiMinorAxis * sinf(theta), 0);
+		theta += step;
+	}
+	return vertices;
+}
 
 int main() {
 	InitWindow(800, 450, "Window");
@@ -26,9 +43,16 @@ int main() {
 		ClearBackground(RAYWHITE);
 		BeginMode3D(camera);
 
-		DrawModel(sphereModel, Vector3{ 0.0f, 0.0f, 0.0f }, 1.0f, WHITE);
+		DrawModel(sphereModel, Vector3{ 0.0f, 0.0f, 0.0f }, RENDER_SCALE * EARTH_EQUATORIAL_RADIUS, WHITE);
 		DrawLine3D(Vector3{}, Vector3{ 5.0, 0.0, 0.0 }, RED);
 		DrawLine3D(Vector3{}, Vector3{ 0.0, 0.0, 5.0 }, BLUE);
+		DrawLine3D(Vector3{}, Vector3{ 0.0, 5.0, 0.0 }, GREEN);
+
+		auto vertices{ ellipse(6793000 * RENDER_SCALE, 6793000 * RENDER_SCALE) };
+		for(int i = 0; i < vertices.size() - 1; ++i) {
+			DrawLine3D(*(Vector3*)(&vertices[i]), *(Vector3*)(&vertices[i + 1]), RED);
+		}
+		DrawLine3D(*(Vector3*)(&vertices[0]), *(Vector3*)(&vertices[vertices.size() - 1]), RED);
 
 		EndMode3D();
 		EndDrawing();
